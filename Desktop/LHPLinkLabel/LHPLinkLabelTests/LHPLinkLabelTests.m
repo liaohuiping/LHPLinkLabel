00bb77d37b30fb3a37f82627f2e67aad035ac35f
lhp <lhp@lhpdeiMac.local>
2018-05-09 13:57:17 +0800

    上传项目源代码

---
+++ Desktop/LHPLinkLabel/LHPLinkLabelTests/LHPLinkLabelTests.m
@@ -0,0 +1,39 @@
+//
+//  LHPLinkLabelTests.m
+//  LHPLinkLabelTests
+//
+//  Created by lhp on 2018/5/7.
+//  Copyright © 2018年 lhp. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LHPLinkLabelTests : XCTestCase
+
+@end
+
+@implementation LHPLinkLabelTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
